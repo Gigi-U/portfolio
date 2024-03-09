7abc3400231d2226685959cf48e32ae94a11f064
Gigi-U <andanlibres@gmail.com>
2024-03-09 17:56:37 -0300

    cambiando about

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,7 @@ import { ContactComponent } from '../contact/contact.component';
   standalone: true,
   imports: [CommonModule, AboutComponent,ProjectsComponent, SkillsComponent, ContactComponent],
   template: `  
+
    <!-- <div class="popup-overlay" *ngIf="isDevelopmentMode">
     <div class="popup-container">
       <span>🚧Under Development👷🏽‍♀️🔧</span>
@@ -23,7 +24,8 @@ import { ContactComponent } from '../contact/contact.component';
   <br><b>
   <p>Stuff I listen to while coding</p><br>
   <iframe width="100%"  src="https://www.youtube.com/embed/playlist?list=PLJ3gHjGOJjav2pStUQn5-REejgnvLZKIv&si=860xmzyj4neib4Xx" frameborder="0" allowfullscreen></iframe>
- `, 
+ `
+ , 
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
